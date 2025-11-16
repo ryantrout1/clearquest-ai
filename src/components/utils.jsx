58e9a8e644b2244ae99846770c3e5f17f9ce7bb2
Ryan Trout <rttg123@gmail.com>
2025-11-16 04:36:53 +0000

    Updated shadcn UI imports

--- src/components/utils.jsx
+++
@@ -1,9 +0,0 @@
-// URL utilities for Base44 navigation
-export function createPageUrl(pageName) {
-  // Remove .js extension if present
-  const cleanName = pageName.replace(/\.js$/, '');
-  return `/${cleanName}`;
-}
-
-// Re-export for compatibility
-export default { createPageUrl };
--- src/components/utils/index.jsx
+++
@@ -1,6 +0,0 @@
-// URL utilities
-export function createPageUrl(pageName) {
-  // Remove .js extension if present
-  const cleanName = pageName.replace(/\.js$/, '');
-  return `/${cleanName}`;
-}
