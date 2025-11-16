2811e08c342848a677a4c555d2d5dc33fc11b733
Ryan Trout <rttg123@gmail.com>
2025-11-16 04:37:09 +0000

    Fix cn import errors

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,10 +1,10 @@
 import * as React from "react"
-import { cva } from "class-variance-authority"
+import { cva } from "class-variance-authority";
 
-import { cn } from "@/components/lib/utils"
+import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
   {
     variants: {
       variant: {
@@ -24,8 +24,7 @@ const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
     ref={ref}
     role="alert"
     className={cn(alertVariants({ variant }), className)}
-    {...props}
-  />
+    {...props} />
 ))
 Alert.displayName = "Alert"
 
@@ -33,8 +32,7 @@ const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
   <h5
     ref={ref}
     className={cn("mb-1 font-medium leading-none tracking-tight", className)}
-    {...props}
-  />
+    {...props} />
 ))
 AlertTitle.displayName = "AlertTitle"
 
@@ -42,8 +40,7 @@ const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("text-sm [&_p]:leading-relaxed", className)}
-    {...props}
-  />
+    {...props} />
 ))
 AlertDescription.displayName = "AlertDescription"
 
