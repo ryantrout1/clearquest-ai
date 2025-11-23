56d0aa2ea14e7bf252152ba46e8b8da8cb067fee
Ryan Trout <rttg123@gmail.com>
2025-11-23 19:58:50 +0000

    File changes

--- src/components/interview/StartResumeMessage.jsx
+++ src/components/interview/StartResumeMessage.jsx
@@ -48,8 +48,7 @@ export default function StartResumeMessage({
               size="lg"
               className="bg-blue-600 hover:bg-blue-700 text-white px-8 h-12"
             >
-              <Shield className="w-5 h-5 mr-2" />
-              Start Interview
+              Next
             </Button>
           </div>
         </div>
