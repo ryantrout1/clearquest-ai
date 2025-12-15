5f522de858a84b63cedd6347c13dc99775c372df
Ryan Trout <rttg123@gmail.com>
2025-12-15 21:14:16 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,7 +3,6 @@ import AdminLogin from './pages/AdminLogin';
 import AiSettings from './pages/AiSettings';
 import BackfillSummaries from './pages/BackfillSummaries';
 import candidateinterviewBackup from './pages/CandidateInterview.backup';
-import candidateinterviewBackup from './pages/CandidateInterview.backup';
 import candidateinterviewClean from './pages/CandidateInterview_CLEAN';
 import CreateDepartment from './pages/CreateDepartment';
 import DepartmentDashboard from './pages/DepartmentDashboard';
@@ -36,7 +35,6 @@ export const PAGES = {
     "AdminLogin": AdminLogin,
     "AiSettings": AiSettings,
     "BackfillSummaries": BackfillSummaries,
-    "CandidateInterview.backup": candidateinterviewBackup,
     "CandidateInterview.backup": candidateinterviewBackup,
     "CandidateInterview_CLEAN": candidateinterviewClean,
     "CreateDepartment": CreateDepartment,
