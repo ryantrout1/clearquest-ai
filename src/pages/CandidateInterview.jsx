f0942d85be060c0eabf3fa2975994c0aa8b2714d
Ryan Trout <rttg123@gmail.com>
2025-12-16 15:44:46 +0000

    File changes

--- src/pages/CandidateInterview.jsx
+++ src/pages/CandidateInterview.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { base44 } from "@/api/base44Client";
